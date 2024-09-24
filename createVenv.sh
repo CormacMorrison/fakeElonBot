9c4429e2a3556a10f632da43865b8f8abc7ff2b1
Cormac Morrison <cormac.morrison@student.unsw.edu.au>
2024-09-24 18:23:42 +1000

    venv script added and requirements.txt

---
+++ createVenv.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Define the virtual environment directory name
+VENV_DIR="$HOME/fakeElonBot"
+
+if ! python3 -m venv --help > /dev/null 2>&1; then
+    echo "'venv' module is not available. Installing python3-venv..."
+    sudo apt-get install python3-venv -y || pip install virtualenv
+    echo "'venv' module installed."
+fi
+# Check if virtual environment directory exists
+if [ ! -d "$VENV_DIR" ]; then
+    echo "Virtual environment not found, creating one..."
+    
+    # Create virtual environment
+    python3 -m venv $VENV_DIR
+    
+    echo "Virtual environment created."
+else
+    echo "Virtual environment already exists."
+fi
+
+# Activate the virtual environment
+source $VENV_DIR/bin/activate
+
+echo "Virtual environment activated."
